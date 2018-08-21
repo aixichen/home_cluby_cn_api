711a8c26bc7621d81ec27cb4e43164bece5196be
mandarava <927395536@qq.com>
2018-08-21 15:22:55 +0800

    init

--- app/service/account.js
+++ app/service/account.js
@@ -7,20 +7,33 @@ class AccountService extends Service {
     const offset = pageSize * (currentPage - 1);
     const limit = pageSize;
     const result = {};
-
-    const result_list = await this.ctx.model.Account.findAll({
+    const amount_type = await this.ctx.model.AccountAmountType.findAll({
+      where: query,
+    }).then(result => {
+      const temp_result = [];
+      result.forEach(result_value => {
+        temp_result[result_value.id] = result_value.amount_type_name;
+      });
+      return temp_result;
+    });
+    result.list = await this.ctx.model.Account.findAll({
       where: query,
       offset,
       limit,
       order: [[ 'amount_date', 'desc' ], [ 'updated_at', 'desc' ]],
+    }).then(result => {
+     return result.map(item =>{
+       const temp_amount_type_cn=amount_type[item.amount_type];
+        return {
+          ...item.toJSON(),
+          amount_type_cn:temp_amount_type_cn
+        };
+     });
     });
 
-    result_list.forEach(item => {
-      item.amount_type_cn = '1';
-    });
+   
     const tempTotal = await this.ctx.model.Account.count({ where: query });
-    result.list = result_list;
-    result.test = '3';
+    
     result.pagination = {};
     result.pagination.pageSize = pageSize;
     result.pagination.currentPage = currentPage;
